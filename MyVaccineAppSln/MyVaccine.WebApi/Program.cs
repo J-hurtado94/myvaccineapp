46b56b7c0aa3e6f8ca4723ab96436df7908ee929
J-hurtado94 <juanhurtado2994@gmail.com>
2023-11-27 02:45:23 -0500

    project until allergyController

--- MyVaccineAppSln/MyVaccine.WebApi/Program.cs
+++ MyVaccineAppSln/MyVaccine.WebApi/Program.cs
@@ -1,12 +1,22 @@
+using FluentValidation.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+using MyVaccine.WebApi.Configurations;
+using MyVaccine.WebApi.Literals;
+using MyVaccine.WebApi.Models;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddFluentValidation(fv => fv.RegisterValidatorsFromAssembly(Assembly.GetExecutingAssembly()));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.SetDataBaseConfiguration();
+builder.Services.SetMyVaccineAuthConfiguration();
+builder.Services.SetDependencyInjection();
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,6 +28,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
